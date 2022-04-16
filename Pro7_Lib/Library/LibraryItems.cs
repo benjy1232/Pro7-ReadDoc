2927ca4562e1edd8217103fe819c77d1f9663c37
Ben Serrano <b.serrano8970@gmail.com>
2022-04-16 17:38:59 -0500

    Update gitignore

---
+++ Pro7_Lib/Library/LibraryItems.cs
@@ -0,0 +1,6 @@
+namespace Pro7_Lib.Library;
+
+public class ProLibraryNames
+{
+    public Dictionary<string, ProID> Presentations { get; set; }
+}
---
+++ Pro7_Lib/Library/ProLibrary.cs
@@ -0,0 +1,6 @@
+namespace Pro7_Lib;
+
+public class ProLibrary
+{
+    public List<ProID>? items { get; set; }
+}
