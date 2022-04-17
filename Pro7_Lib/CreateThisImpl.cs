3366f6152f10d1c5ec8fc96ffaafe900d94bf71a
Ben Serrano <b.serrano8970@gmail.com>
2022-04-17 12:01:12 -0500

    Add ability to create a playlist/playlistgroup

---
+++ Pro7_Lib/CreateThisImpl.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pro7_Lib
+{
+    public class CreateThisImpl
+    {
+        public CreateThis createdItem { get; }
+        public CreateThisImpl(string name, string type, string webRoot)
+        {
+            createdItem = new CreateThis(name, type);
+            string ToSend = JsonConvert.SerializeObject(createdItem, Formatting.Indented);
+            StringContent Sending = new(ToSend, Encoding.UTF8, "application/json");
+            HttpClient client = new HttpClient();
+            Task<HttpResponseMessage> PostTask = client.PostAsync($"{webRoot}/v1/playlists", Sending);
+            while(!PostTask.IsCompleted)
+            {
+                Thread.Sleep(1000);
+            }
+            HttpResponseMessage result = PostTask.Result;
+            string res = result.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(res);
+
+        }
+    }
+}
--- Testing/UnitTest1.cs
+++ Testing/UnitTest1.cs
@@ -1,14 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using Pro7_Lib;
 using Pro7_Lib.Library;
+using Pro7_Lib.Playlist;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Testing;
 
 public class Tests
 {
+
     [SetUp]
     public void Setup()
     {
@@ -17,11 +21,18 @@ public class Tests
     [Test]
     public void GetLibrary()
     {
-        LibraryItems lib = new LibraryItems("http://192.168.0.34:50001");
+        LibraryItems lib = new LibraryItems("http://192.168.0.127:1025");
         if (lib.Presentations == null)
         {
             Assert.Fail();
         }
+        Assert.Pass();
+    }
+
+    [Test]
+    public void AddPlaylist()
+    {
+        CreateThisImpl create = new("Easter", "playlist", "http://192.168.0.127:1025");
         Assert.Pass();
     }
 }
